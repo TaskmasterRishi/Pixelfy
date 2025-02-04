b9d937898c682902ec4eba1d25f31cffc86df8bb
Rishi Patodiya <rishipatodiya12@gmail.com>
2025-02-04 16:03:31 +0530

    creat post ui

--- src/app/(tabs)/index.jsx
+++ src/app/(tabs)/index.jsx
@@ -2,14 +2,14 @@ import { FlatList } from "react-native";
 import posts from "../../../assets/data/post.json";
 import PostListItem from "~/src/Components/PostListItem";
 
+
 export default function FeedScreen() {
   return (
     <>
       <FlatList
         data={posts}
-        className="flex "
         renderItem={({ item }) => <PostListItem post={item} />}
-        contentContainerStyle={{gap:10, maxWidth: 512, width: "100%"}}
+        contentContainerStyle={{gap:10, maxWidth: 512,alignSelf: "center" , width: "100%"}}
         showsVerticalScrollIndicator={false}
       />
     </>
--- src/app/(tabs)/new.jsx
+++ src/app/(tabs)/new.jsx
@@ -1,5 +1,36 @@
-import { Text } from "react-native";
+import { Text, View, Image, TextInput, Pressable } from "react-native";
+import { useState } from "react";
 
-export default function CreatePost(){
-    return <Text>Create Post</Text>
+export default function CreatePost() {
+  const [caption, setCaption] = useState("");
+  return (
+    <View className="p-3 items-center flex-1">
+      {/* Image Picker */}
+      <Image
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+        }}
+        className="w-60 aspect-[3/4] rounded-xl shadow-black bg-slate-300"
+      />
+
+      <Text onPress={() => {}} className="text-blue-500 font-semibold m-5">
+        Change
+      </Text>
+      {/* TextInput for caption */}
+
+      <TextInput
+        value={caption}
+        onChangeText={(newValue) => setCaption(newValue)}
+        placeholder="What's on your mind?"
+        className="w-full p-3"
+      />
+
+      {/* Button */}
+      <View className="mt-auto w-full">
+        <Pressable className="bg-blue-500 w-full p-4 items-center rounded-xl">
+          <Text className="text-white font-bold">Share</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
