4409b463fd3d79abdfd4100e6dfd2168e7690f52
Rishi Patodiya <rishipatodiya12@gmail.com>
2025-04-04 02:00:06 +0530

    Setup chat commit setup

--- src/app/(tabs)/(chat)/channel/[cid].tsx
+++ src/app/(tabs)/(chat)/channel/[cid].tsx
@@ -1,31 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, Text } from 'react-native';
-import { Channel, MessageInput, MessageList, useChannelContext } from 'stream-chat-expo';
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Text } from "react-native";
+import {
+  Channel,
+  MessageInput,
+  MessageList,
+  useChannelContext,
+} from "stream-chat-expo";
 import { Channel as ChannelType, StreamChat } from "stream-chat";
-import { useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const client = StreamChat.getInstance("cxc6zzq7e93f");
 
 export default function ChannelScreen() {
-    const [channel, setChannel] = useState<ChannelType>();
-    const {cid} = useLocalSearchParams<{cid : string}>();
+  const [channel, setChannel] = useState<ChannelType | null>(null);
+  const { cid } = useLocalSearchParams<{ cid: string }>();
 
-    useEffect(() => {
-        const fetchChannel = async () => {
-            const channels = await client.queryChannels({cid});
-            setChannel(channels[0])
-        };
-        fetchChannel();
-    },[cid]);
+  useEffect(() => {
+    const fetchChannel = async () => {
+      const channels = await client.queryChannels({ cid });
+      setChannel(channels[0]);
+    };
+    fetchChannel();
+  }, [cid]);
 
-    if (!channel) {
-        return <ActivityIndicator />;
-    }
+  if (!channel) {
+    return <ActivityIndicator />;
+  }
 
-    return (
-        <Channel channel={channel}>
-          <MessageList />
-          <MessageInput />
-        </Channel>
-    );
+  return (
+    <Channel channel={channel}>
+      <MessageList />
+      <SafeAreaView edges={["bottom"]}></SafeAreaView>
+      <MessageInput />
+    </Channel>
+  );
 }
